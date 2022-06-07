7fd7c49f6d88c83bf289a94e34d4a129df19fc7c
sanu <shanuksrivastava@gmail.com>
2022-06-07 19:57:41 +0530

    second assiment

---
+++ src/loggers/logger.js
@@ -0,0 +1,4 @@
+function welcome (){
+console.log("Welcome to my application. I am SHANU and a part of FunctionUp radon cohort.")
+}
+module.exports.welcome= welcome
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,12 @@
-const express = require('express');
+const express = require("express");
+const logger = require("../loggers/logger.js")
+const helper = require("../util/helper")
+const validator = require("../validator/formetter.js")
+//  const router = express.Router();
+var _ = require('lodash');
+
+// const express = require('express');
+
 const externalModule = require('./logger')
 
 const router = express.Router();
@@ -24,6 +32,33 @@ router.get('/test-me3', function (req, res) {
 
 router.get('/test-me4', function (req, res) {
     res.send('My last api!')
+});
+
+// chunk function
+router.get('/chunk', function (req, res) {
+    const chunk = (arr, size) =>
+    Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+        arr.slice(i * size, i * size + size)
+    );
+    console.log(chunk([1, 2, 3, 4, 5, 5, 7, 8 ,3 ,6 , 7], 5));
+    res.send(chunk(['Jan', 'Feb', 3, 4, 5, 5, 7, 8 ,3 ,6 , 7], 5));
+});
+
+// chunk function
+router.get('/tail', function (req, res) {
+    const _ = require('lodash');
+    n=20;
+    var a =[];
+  for(i=n-1;i>=0;i--){
+      a.push(1+i*2);
+  }
+
+    // let x = [1,3,5,7,9,11,13,15,17,19]
+    
+    let newArray = _.tail(a);
+    
+    console.log(newArray);
+    res.send(newArray)
 });
 
 module.exports = router;
